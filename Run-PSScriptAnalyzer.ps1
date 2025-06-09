6379818d465170203eee43ec4662e70c5f36ca94
Alan Płócieniak <alan-null@outlook.com>
2025-06-09 11:14:43 +0200

    [ci] PSScriptAnalyzerSettings rename to avoid conflicts

--- Run-PSScriptAnalyzer.ps1
+++ Run-PSScriptAnalyzer.ps1
@@ -2,4 +2,4 @@ Import-Module PSScriptAnalyzer
 
 $Public = Get-ChildItem -Path $PSScriptRoot -Recurse -Filter "*.ps1" | ? { $_.Name[0].Equals($_.Name.ToUpper()[0]) }
 
-$Public | % { Invoke-ScriptAnalyzer -Path $_.fullname -Settings .\PSScriptAnalyzerSettings.psd1 }
+$Public | % { Invoke-ScriptAnalyzer -Path $_.fullname -Settings .\PSScriptAnalyzerSettings.ps1 }
